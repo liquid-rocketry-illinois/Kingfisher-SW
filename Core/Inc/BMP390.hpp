50ec3f29904fab27128f2ae644ee83fcb1c673a9
koline2024 <christopherxia2020@gmail.com>
2025-11-23 16:19:41 -0600

    edited BMP390 code and started on read function

--- Core/Inc/BMP390.hpp
+++ Core/Inc/BMP390.hpp
@@ -1,3 +1,11 @@
 #pragma once
 #include "lib\BMP3_SensorAPI-master\BMP3_SensorAPI-master\bmp3.h"
 
+
+/**
+*@brief initBMP390 initialises the BMP390 temperature and pressure sensor
+*with the FIFO data structure. 
+*@return 1 if successful, 0 if error. 
+*
+*/
+int initBMP390();
--- Core/Src/BMP390.cpp
+++ Core/Src/BMP390.cpp
@@ -1,11 +1,14 @@
 #include "BMP3xx_platform.cpp"
+#include <exception>
 #define FIFO_WATERMARK_FRAME_COUNT  UINT8_C(50)
 #define FIFO_MAX_SIZE               UINT16_C(512)
 
-int readData(){
+static struct bmp3_dev dev;       // Device handle
+
+
+int initBMP390(){
     // Variable declarations below
-    struct bmp3_dev dev;       // Device handle
-    struct bmp3_spi_intf;      // Data struct
+    struct bmp3_spi_intf;      // SPI Interface
     int8_t rslt;               // Result code called by driver
     uint16_t settings_sel;     // Settings selection
     uint16_t settings_fifo;    //  FIFO selection 
@@ -18,9 +21,38 @@ int readData(){
     struct bmp3_fifo_data fifo = { 0 };
     struct bmp3_status status = { { 0 } };
 
-    rslt = 
+    try{
+    rslt = bmp3_interface_init(&dev, BMP3_SPI_INTF);
+    // TODO: Write a check result function
+    rslt = bmp3_init(&dev);
+    // Implement check function
 
+    // Config FIFO settings to FIFO both pressure and temperature
+    fifo_settings.mode = BMP3_DISABLE;
+    
+    settings.press_en = BMP3_ENABLE;
+    settings.temp_en = BMP3_ENABLE;
+    settings.odr_filter.press_os = BMP3_OVERSAMPLING_2X;
+    settings.odr_filter.temp_os = BMP3_OVERSAMPLING_2X;
+    settings.odr_filter.odr = BMP3_ODR_12_5_HZ;
+    settings.int_settings.latch = BMP3_ENABLE;
 
+    settings_sel = BMP3_SEL_PRESS_EN | BMP3_SEL_TEMP_EN | BMP3_SEL_PRESS_OS | BMP3_SEL_TEMP_OS | BMP3_SEL_ODR;
 
+    settings.op_mode = BMP3_MODE_NORMAL;
+
+    // Add check statements to each of the following
+    rslt = bmp3_set_op_mode(&settings, &dev);
+    rslt = bmp3_set_sensor_settings(settings_sel, &settings, &dev);
     return 1;
+
+    }catch(std::exception e){
+        return 0;
+    }
+}
+
+void readBMP390(){
+
+    static struct bmp3_data data;
+    
 }
