c0b7533e9d2c1073cb6e36ac16d600b1fa65d060
Christopher Xia <christopherxia2020@gmail.com>
2026-04-02 20:16:12 -0500

    Testing GPS code rn

--- Core/Src/MAXM10S.cpp
+++ Core/Src/MAXM10S.cpp
@@ -4,7 +4,7 @@
 
 #include "MAXM10S.h"
 
-
+#include "i2c.h"
 
 
 MAXM10S::MAXM10S(I2C_HandleTypeDef *hi2c) {
@@ -13,10 +13,12 @@ MAXM10S::MAXM10S(I2C_HandleTypeDef *hi2c) {
 
 uint16_t MAXM10S::getAvailableBytes() {
     uint8_t buffer[2] = {0}; // To store high and low bits
-    HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(_hi2c, I2C_ADDRESS, REG_BYTES_AVAIL, I2C_MEMADD_SIZE_8BIT, buffer, 2, 100);
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(_hi2c, I2C_ADDRESS, REG_BYTES_AVAIL, I2C_MEMADD_SIZE_8BIT, buffer, 2, 1000);
+
     if (status == HAL_OK) {
         return (buffer[0] << 8) | buffer[1];
     }
+
 return 0;
 }
 
@@ -25,8 +27,13 @@ uint16_t MAXM10S::readGPS(uint16_t numBytes, uint8_t *buffer) {
         return -1;
     }
 
-    HAL_I2C_Mem_Read(_hi2c, I2C_ADDRESS, REG_DATA_STREAM, I2C_MEMADD_SIZE_8BIT, buffer, numBytes, 1000);
-    return 0; // For successful read
+    HAL_StatusTypeDef status = HAL_I2C_Master_Receive(_hi2c, I2C_ADDRESS, buffer, numBytes, 1000);
+
+    if (status == HAL_OK) {
+        return 1; // The buffer is now full of GPS data!
+    }
+
+    return 0; // should not get this
 }
 
 
