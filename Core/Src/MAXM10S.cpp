db015b8091ea91f38df4edc28f58c28ff50c6272
jasonfz2025-jpg <jasonfz2025@gmail.com>
2026-04-02 21:50:22 -0500

    Working reading GPS functioning code

--- Core/Src/MAXM10S.cpp
+++ Core/Src/MAXM10S.cpp
@@ -9,6 +9,7 @@
 
 MAXM10S::MAXM10S(I2C_HandleTypeDef *hi2c) {
     _hi2c = hi2c;
+    HAL_GPIO_WritePin(GPS_RST_GPIO_Port, GPS_RST_Pin, GPIO_PIN_SET);
 }
 
 uint16_t MAXM10S::getAvailableBytes() {
@@ -27,7 +28,7 @@ uint16_t MAXM10S::readGPS(uint16_t numBytes, uint8_t *buffer) {
         return -1;
     }
 
-    HAL_StatusTypeDef status = HAL_I2C_Master_Receive(_hi2c, I2C_ADDRESS, buffer, numBytes, 1000);
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(_hi2c, I2C_ADDRESS, REG_DATA_STREAM, I2C_MEMADD_SIZE_8BIT, buffer, numBytes+1, 1000);
 
     if (status == HAL_OK) {
         return 1; // The buffer is now full of GPS data!
--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -16,23 +16,24 @@ extern "C" void task(void*) {
     MICROS_DWT_Timebase_Init(); // Initialize micros() timer
     TEST test;
     MAXM10S gps(&hi2c4);
-    uint8_t gpsBuffer[1024]; // Buffer to hold the incoming data
+    osDelay(1000);
+    static uint8_t gpsBuffer[2048]; // Buffer to hold the incoming data
 
     /* Infinite loop */
     for(;;)
     {
-        for (int i = 0; i < 3; i++) {
-            HAL_GPIO_TogglePin(USR_LED_GPIO_Port, USR_LED_Pin);
-        }
         uint16_t bytesWaiting = gps.getAvailableBytes();
-        if (bytesWaiting > 0) {
+        if (bytesWaiting > 50) {
             if (bytesWaiting > sizeof(gpsBuffer)) {
                 bytesWaiting = sizeof(gpsBuffer);
             }
-            gps.readGPS(bytesWaiting, gpsBuffer);
+            if (gps.readGPS(bytesWaiting, gpsBuffer) > 0)
+            {
+                int breakPoint = 0;
+            }
         }
         // wont get past here for tests
-        osDelay(1);
+        vTaskDelay(100);
     }
     /* USER CODE END StartDefaultTask */
 }
